2b301693f5b997cfc6cdf08fdc07f220be4e9dbd
zedlj <zoelauren-jones@hotmail.co.uk>
2022-03-27 21:52:32 +0100

    picture in picture functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
 
+// Prompt to select media stream, pass to video element, plan
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        console.log(videoElement)
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset pictures if picture in picture successful
+    button.disabled = false;
+});
+
+selectMediaStream();
